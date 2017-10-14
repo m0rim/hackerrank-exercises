55078fcb48b8d08f6958fd37ad3a838ecb0b0fe2
Sysco <naru@protonmail.com>
2017-10-14 14:11:42 +0100

    Arrays Introduction

---
+++ Cpp/Introduction/array.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    int cnt;
+    int *arrayofn = nullptr;
+    
+    cin >> n;
+    
+    arrayofn = (int *) malloc(n * sizeof(n));
+    if(arrayofn == nullptr)
+        return -1;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> arrayofn[i];
+    }
+    for (int i = n-1; i >= 0; i--) {
+        cout << arrayofn[i] << " ";   
+    }
+    
+    return 0;
+}
